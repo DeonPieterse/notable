208f7a34e088b5dd51216449b3cd2e5a20e8f39e
Deon <Gideondeonjacobus@Gmail.com>
2020-04-15 23:18:16 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    url: "mongodb+srv://MongoUser:pass1234@azurecluster1-9f61f.azure.mongodb.net/test?retryWrites=true&w=majority",
+    url: "mongodb+srv://<Username>:<Password>@azurecluster1-9f61f.azure.mongodb.net/test?retryWrites=true&w=majority",
     name: "AzureCluster1"
 }
